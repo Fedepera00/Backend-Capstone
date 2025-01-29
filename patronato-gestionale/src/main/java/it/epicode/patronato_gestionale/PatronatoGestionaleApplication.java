b1bc556d66acbd9859dcfad2c91a8bf436d90d15
Federico Peralta <federico.peralta91@gmail.com>
2025-01-29 15:36:52 +0100

    1 Commit progetto backend

---
+++ patronato-gestionale/src/main/java/it/epicode/patronato_gestionale/PatronatoGestionaleApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.patronato_gestionale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronatoGestionaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronatoGestionaleApplication.class, args);
+	}
+
+}
---
+++ patronato-gestionale/src/test/java/it/epicode/patronato_gestionale/PatronatoGestionaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.patronato_gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronatoGestionaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
